28c0d95250e5a54242376f6f2e3c5e1b7dc6999b
Adriano Rutz <adriano.rutz@hotmail.com>
2024-11-04 13:54:15 +0100

    feat(ui): add qids parameter to prettyTables_progress function

--- R/prettyTables_progress.R
+++ R/prettyTables_progress.R
@@ -1,27 +1,29 @@
 #' Title
 #'
 #' @param xs
+#' @param qids
 #'
 #' @return NULL
 #' @export
 #'
 #' @examples NULL
-prettyTables_progress <- function(xs) {
+prettyTables_progress <- function(xs, qids) {
   p <- progressr::progressor(along = xs)
   future.apply::future_lapply(
     X = xs,
-    FUN = function(x) {
+    FUN = function(x, qids) {
       p()
       temp_gt_function(
         table = x,
         title = paste(
           "Compounds found in",
-          names(params$organisms$wikidata),
+          names(qids[[1]]),
           "on ",
           as.Date(Sys.Date(), "%m/%d/%y")
         ),
         subtitle = "All compounds"
       )
-    }
+    },
+    qids = qids
   )
 }
