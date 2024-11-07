372d09f940584fb08e031918e02ac594bee9fc53
Adriano Rutz <44283913+Adafede@users.noreply.github.com>
2024-11-07 07:25:35 +0100

    refactor(progress): improve function performance and readability

--- R/prettyTables_progress.R
+++ R/prettyTables_progress.R
@@ -9,21 +9,21 @@
 #' @examples NULL
 prettyTables_progress <- function(xs, qids) {
   p <- progressr::progressor(along = xs)
-  future.apply::future_lapply(
-    X = xs,
-    FUN = function(x, qids) {
-      p()
-      temp_gt_function(
-        table = x,
-        title = paste(
-          "Compounds found in",
-          names(qids[[1]]),
-          "on ",
-          as.Date(Sys.Date(), "%m/%d/%y")
-        ),
-        subtitle = "All compounds"
-      )
-    },
-    qids = qids
-  )
+  xs |>
+   furrr::map(
+     .fun = function(x, qids) {
+       p()
+       temp_gt_function(
+         table = x,
+         title = paste(
+           "Compounds found in",
+           names(qids[[1]]),
+           "on ",
+           as.Date(Sys.Date(), "%m/%d/%y")
+           ),
+         subtitle = "All compounds"
+         )
+       },
+     qids = qids
+     )
 }
